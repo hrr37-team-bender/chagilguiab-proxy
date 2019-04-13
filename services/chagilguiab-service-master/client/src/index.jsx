2cb4c008b142d535fd1704be064d8e4a6bcca4f0
Chagil Guiab <chagilguiab@gmail.com>
2019-04-12 18:26:41 -0700

    removes jquery from index.jsx in chagil service

--- services/chagilguiab-service-master/client/src/index.jsx
+++ services/chagilguiab-service-master/client/src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Descriptions from './components/Home.jsx';
-import $ from 'jquery';
 
 ReactDOM.render(<Descriptions />, document.getElementById('Descriptions') || document.createElement('div'));
